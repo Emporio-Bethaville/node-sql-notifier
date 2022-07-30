49281ba99c8a2d94ec9a9578493cee7bf140b3b8
Eiji Tomonari <gabriel@tomonari.dev>
2022-07-30 13:24:35 -0300

    Initial server

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
-//Load HTTP module
-const http = require("http");
-const hostname = "127.0.0.1";
-const port = 3000;
+const sql = require("mssql");
+const dotenv = require("dotenv");
+dotenv.config();
+const sqlConfig = {
+  port: 1433,
+  server: "localhost",
+  authentication: {
+    type: "default",
+    options: {
+      userName: process.env.DB_USER,
+      password: process.env.DB_PWD,
+    },
+  },
+  options: {
+    database: process.env.DB_NAME,
+    validateBulkLoadParameters: false,
+    encrypt: false,
+  },
+};
 
-//Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
+sql.connect(sqlConfig, (err) => {
+  // ... error checks
+  console.log(err);
+  const request = new sql.Request();
+  request.stream = true; // You can set streaming differently for each request
+  request.query("select * from dbo.itensComandas"); // or request.execute(procedure)
+
+  request.on("recordset", (columns) => {
+    // Emitted once for each recordset in a query
+  });
+
+  request.on("row", (row) => {
+    // Emitted for each row in a recordset
+    console.log(row);
+  });
+
+  request.on("rowsaffected", (rowCount) => {
+    // Emitted for each INSERT, UPDATE, or DELETE statement in a batch.
+  });
+
+  request.on("error", (err) => {
+    // May be emitted multiple times
+  });
+
+  request.on("done", (result) => {
+    // Always emitted as the last one
+  });
 });
 
-//listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+sql.on("error", (err) => {
+  // ... error handler
 });
