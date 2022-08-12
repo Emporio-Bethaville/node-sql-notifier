ab09ae21ae9b3be5e6254ff7059e273430d83bf9
GabrielTomonariDev <gabriel@tomonari.dev>
2022-08-12 13:43:38 -0300

    Add insertion if unit is KG

--- queries/afterInsertTrigger.sql
+++ queries/afterInsertTrigger.sql
@@ -1,68 +1,134 @@
-CREATE TRIGGER trgAfterInsert ON [dbo].[mt_Itens] FOR
-INSERT
-    AS declare @id int;
+CREATE TRIGGER trgAfterInsert ON [ dbo ].[ mt_Itens ] FOR INSERT AS declare @ id int;
 
-declare @date datetime;
+declare @ date datetime;
 
-declare @quantity float;
+declare @ quantity float;
 
-declare @person varchar(50);
+declare @ person varchar(50);
 
-declare @microterminal varchar(50);
+declare @ microterminal varchar(50);
 
-declare @dscrpt varchar(29);
+declare @ dscrpt varchar(29);
 
-declare @details nvarchar(225);
+declare @ details nvarchar(225);
 
-declare @productId int;
+declare @ productId int;
 
-declare @itemNumber int;
+declare @ itemNumber int;
 
-declare @sector int;
+declare @ sector int;
+
+declare @ tableId int;
+
+declare @ unit varchar(2);
 
 select
-    @id = i.idComanda,
-    @date = i.dtData,
-    @quantity = i.nrQuantidade,
-    @person = i.stOperador,
-    @microterminal = i.idMicroterminal,
-	@productId = i.idProduto,
-    @details = i.stIncremento,
-	@itemNumber = i.nrItem
-    
+  @ id = i.idComanda,
+  @ date = i.dtData,
+  @ quantity = i.nrQuantidade,
+  @ person = i.stOperador,
+  @ microterminal = i.idMicroterminal,
+  @ productId = i.idProduto,
+  @ details = i.stIncremento,
+  @ itemNumber = i.nrItem,
+  @ unit = i.medida,
 from
-    inserted i;
+  inserted i;
 
-select @dscrpt = (SELECT stProduto FROM [NATI2].[dbo].[prd_Produtos] where idProduto=@productId)
-select @sector = (SELECT idPrint FROM [NATI2].[dbo].[mt_ProdutosPrint] where idProduto = @productId and idMicroterminal = @microterminal);
+select
+  @ dscrpt = (
+    SELECT
+      stProduto
+    FROM
+      [ NATI2 ].[ dbo ].[ prd_Produtos ]
+    where
+      idProduto = @ productId
+  )
+select
+  @ sector = (
+    SELECT
+      idPrint
+    FROM
+      [ NATI2 ].[ dbo ].[ mt_ProdutosPrint ]
+    where
+      idProduto = @ productId
+      and idMicroterminal = @ microterminal
+  );
 
-IF @sector IS NOT NULL
-BEGIN
+select
+  @ tableId = (
+    SELECT
+      idMesa
+    FROM
+      [ NATI2 ].[ dbo ].[ mt_Atendimentos ]
+    where
+      idComanda = @ id
+  );
+
+IF @ sector IS NOT NULL BEGIN IF @ unit = 'UN' BEGIN DECLARE @ cnt INT = 0;
+
+WHILE @ cnt < @ quantity BEGIN
 insert into
-    OrdersApp.dbo.itensComandas (
-        id,
-        date,
-        quantity,
-        person,
-        microterminal,
-        dscrpt,
-        details,
-        productId,
-		itemNumber,
-        sector
-    )
+  OrdersApp.dbo.itensComandas (
+    id,
+    date,
+    quantity,
+    person,
+    microterminal,
+    dscrpt,
+    details,
+    productId,
+    itemNumber,
+    sector,
+    tableId
+  )
 values
-    (
-        @id,
-        @date,
-        @quantity,
-        @person,
-        @microterminal,
-        @dscrpt,
-        @details,
-        @productId,
-		@itemNumber,
-        @sector
-    )
+  (
+    @ id,
+    @ date,
+    1,
+    @ person,
+    @ microterminal,
+    @ dscrpt,
+    @ details,
+    @ productId,
+    @ itemNumber,
+    @ sector,
+    @ tableId
+  )
+SET
+  @ cnt = @ cnt + 1;
+
 END
-GO
+ELSE BEGIN
+insert into
+  OrdersApp.dbo.itensComandas (
+    id,
+    date,
+    quantity,
+    person,
+    microterminal,
+    dscrpt,
+    details,
+    productId,
+    itemNumber,
+    sector,
+    tableId
+  )
+values
+  (
+    @ id,
+    @ date,
+    @ quantity,
+    @ person,
+    @ microterminal,
+    @ dscrpt,
+    @ details,
+    @ productId,
+    @ itemNumber,
+    @ sector,
+    @ tableId
+  )
+END
+END
+END GO
